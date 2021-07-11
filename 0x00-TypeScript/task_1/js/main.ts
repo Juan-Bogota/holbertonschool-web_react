cf0ca973859ef86c5f93df7e703a4367c03f61f2
Juan Lopez <juan.lopez@triario.com>
2021-07-10 22:41:59 -0500

    Task 1: Writing a class Teacher - Student

---
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -0,0 +1,62 @@
+// 1. Let's build a Teacher interface
+interface Teacher {
+  readonly firstName: string,
+  readonly lastName: string,
+  fullTimeEmployee: boolean,
+  yearsOfExperience?: number,
+  location: string,
+  [property: string]: any;
+};
+
+const teacherOne: Teacher = {
+  firstName: 'Juan',
+  fullTimeEmployee: false,
+  lastName: 'Lopez',
+  location: 'Bogota',
+  contract: false,
+};
+
+// 2. Extending the Teacher class
+interface Directors extends Teacher {
+  numberOfReports: number,
+};
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+// 3. Printing teachers
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+export const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => `${firstName.charAt(0)}. ${lastName}`;
+
+//console.log(printTeacher("John", "Doe"));
+
+// 4. Writing a class
+interface StudentClassConstructor {
+  new(firstName: string, lastName: string): StudentClassInterface;
+}
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+}
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework() {
+    return 'Currently working';
+  }
+  displayName() {
+    return this.firstName;
+  }
+}
