392ec0e25980a44c709dc36c0f73e92272085868
Juan Lopez <juan.lopez@triario.com>
2021-07-11 11:23:47 -0500

    task 0: Basic components

---
+++ 0x03-react_props/task_0/dashboard/src/App/App.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import { Header } from '../Header/Header';
+import { Login } from '../Login/Login';
+import { Footer } from '../Footer/Footer';
+import { Notifications } from '../Notifications/Notifications';
+
+export const App = () => {
+  return (
+    <Fragment>
+      <Notifications />
+      <Header />
+      <Login />
+      <Footer />
+    </Fragment>
+  );
+}
