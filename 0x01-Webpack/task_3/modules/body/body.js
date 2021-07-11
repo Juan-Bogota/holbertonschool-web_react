df2a4ba548f55271937e4d333d4c601734c7bcf6
Juan Lopez <juan.lopez@triario.com>
2021-07-11 00:06:03 -0500

    Task 3: Dev servers, modules, and tree shaking

---
+++ 0x01-Webpack/task_3/modules/body/body.js
@@ -0,0 +1,16 @@
+import $ from 'jquery';
+const _ = require('lodash');
+import './body.css';
+
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+
+function updateCounter() {
+  let times = $('#count').html() || 0;
+  $('button').on('click', () => {
+    $('#count').html(`${++times} clicks on the button`);
+  });
+};
+
+_.debounce(updateCounter(), 500);
