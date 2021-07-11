7d1ba0edf8ad7c576e26a9dfbfa45ee07e789fee
Juan Lopez <juan.lopez@triario.com>
2021-07-10 23:13:36 -0500

    Task 3: Crud

---
+++ 0x00-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,6 @@
+// 8. Ambient Namespaces
+import { RowID, RowElement } from "./interface";
+
+export function insertRow(row: RowElement): number;
+export function deleteRow(rowId: RowID): void;
+export function updateRow(rowId: RowID, row: RowElement): RowID;
