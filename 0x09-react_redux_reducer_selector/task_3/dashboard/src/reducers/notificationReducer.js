d3ae31401efe86a88e20ea471566d762666e865b
Juan Lopez <juan.lopez@triario.com>
2021-08-09 23:34:48 -0500

    3. Create the reducer for notifications

--- 0x09-react_redux_reducer_selector/task_3/dashboard/src/reducers/notificationReducer.js
+++ 0x09-react_redux_reducer_selector/task_3/dashboard/src/reducers/notificationReducer.js
@@ -1,52 +1,41 @@
 import {
-  FETCH_NOTIFICATIONS_SUCCESS,
-  MARK_AS_READ,
-  SET_TYPE_FILTER,
+	FETCH_NOTIFICATIONS_SUCCESS,
+	MARK_AS_READ,
+	SET_TYPE_FILTER,
 } from '../actions/notificationActionTypes';
 
 export const initialNotificationState = {
-  notifications: [],
-  filter: 'DEFAULT',
+	notifications: [],
+	filter: 'DEFAULT',
 };
 
 const notificationReducer = (state = initialNotificationState, action) => {
-  switch (action.type) {
-    /*
-    case FETCH_NOTIFICATIONS_SUCCESS:
-      return {
-        ...state,
-        notifications: action.data.map((notification) => {
-          return {
-            ...notification,
-            isRead: false,
-          };
-        }),
-      };
-      */
+	switch (action.type) {
 
-    case MARK_AS_READ:
-      return {
-        ...state,
-        notifications: state.notifications.map((notification) => {
-          const current = {
-            ...notification,
-          };
-          if (notification.id == action.index) current.isRead = true;
 
-          return current;
-        }),
-      };
+		case MARK_AS_READ:
+			return {
+				...state,
+				notifications: state.notifications.map((notification) => {
+					const current = {
+						...notification,
+					};
+					if (notification.id == action.index) current.isRead = true;
 
-    case SET_TYPE_FILTER:
-      return {
-        ...state,
-        filter: action.filter,
-      };
+					return current;
+				}),
+			};
 
-    default:
-      break;
-  }
-  return state;
+		case SET_TYPE_FILTER:
+			return {
+				...state,
+				filter: action.filter,
+			};
+
+		default:
+			break;
+	}
+	return state;
 };
 
 export default notificationReducer;
