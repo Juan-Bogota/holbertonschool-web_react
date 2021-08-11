16cb5eef8e37401070eaecffded5ce397a5f012a
Juan Lopez <juan.lopez@triario.com>
2021-08-10 20:25:51 -0500

    4. Normalizr & Immutable

--- 0x09-react_redux_reducer_selector/task_4/dashboard/src/schema/notifications.js
+++ 0x09-react_redux_reducer_selector/task_4/dashboard/src/schema/notifications.js
@@ -4,37 +4,37 @@ import { normalize, schema } from 'normalizr';
 const user = new schema.Entity('users');
 
 const message = new schema.Entity(
-  'messages',
-  {},
-  {
-    idAttribute: 'guid',
-  }
+	'messages',
+	{},
+	{
+		idAttribute: 'guid',
+	}
 );
 
 const notification = new schema.Entity('notifications', {
-  author: user,
-  context: message,
+	author: user,
+	context: message,
 });
 
 const normalizedData = normalize(notificationsData.default, [notification]);
 export { normalizedData };
 
 export function getAllNotificationsByUser(userId) {
-  const notifications = normalizedData.entities.notifications;
-  const messages = normalizedData.entities.messages;
-  const notificationsByUser = [];
+	const notifications = normalizedData.entities.notifications;
+	const messages = normalizedData.entities.messages;
+	const notificationsByUser = [];
 
-  for (const property in notifications) {
-    if (notifications[property].author === userId) {
-      notificationsByUser.push(messages[notifications[property].context]);
-    }
-  }
-  return notificationsByUser;
+	for (const property in notifications) {
+		if (notifications[property].author === userId) {
+			notificationsByUser.push(messages[notifications[property].context]);
+		}
+	}
+	return notificationsByUser;
 }
 
 const notificationsNormalizer = (data) => {
-  const normalizedData = normalize(data, [notification]);
-  return normalizedData.entities;
+	const normalizedData = normalize(data, [notification]);
+	return normalizedData.entities;
 };
 
 export default notificationsNormalizer;
